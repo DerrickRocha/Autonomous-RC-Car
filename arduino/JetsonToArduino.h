49c542799622d47f51246b238e1b0c1ede114e67
Derrick Rocha <drocha616@gmail.com>
2016-08-27 14:35:57 -0600

    arduino code

---
+++ arduino/JetsonToArduino.h
@@ -0,0 +1,23 @@
+
+
+const int STOP = 0;
+const int FORWARD = 1;
+const int BACKWARD = 2;
+const int LEFT = 3;
+const int RIGHT = 4;
+const int STEER_STRAIGHT = 5;
+
+
+
+
+void moveForward(int fd);
+
+void moveBackward(int fd);
+
+void steerLeft(int fd);
+
+void steerRight(int fd);
+
+void steerStraight(int fd);
+
+void sendToArduino(int fd, int direction);
